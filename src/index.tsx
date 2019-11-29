aabe829d07029f9e04763c4c32bc329454c34621
John <john.19col@gmail.com>
2019-11-29 17:54:41 -0500

    Project setup

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+const App = () => <div>Hi there!</div>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
