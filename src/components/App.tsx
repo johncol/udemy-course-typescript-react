dd804f7b106635b8b616e57e62a60a6fc830d87f
John <john.19col@gmail.com>
2019-12-02 12:05:07 -0500

    Connect App component with redux store

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,48 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export const App = (): JSX.Element => {
-  return <div>Hi there</div>;
+import { Todo } from './../models/todo';
+import { StoreState } from './../state/reducers';
+import { fetchTodos } from './../state/actions';
+import './app.css';
+
+interface PropsState {
+  todos: Todo[];
+}
+
+interface PropsActions {
+  fetchTodos: () => void;
+}
+
+interface Props extends PropsState, PropsActions {}
+
+class AppComponent extends React.Component<Props, {}> {
+  render = (): JSX.Element => {
+    return (
+      <div className="todos">
+        <h1 className="todos__title">Just another TODOs Sample App</h1>
+        <button className="todos__btn-fetch" onClick={this.props.fetchTodos}>
+          Click here to fetch the TODOs list
+        </button>
+        <ul className="todos__list">
+          {this.props.todos.map((todo: Todo) => {
+            return (
+              <li key={todo.id} className="todos__item">
+                <span className="todos__item-title">{todo.title}</span>
+                <button className="todos__item-btn-delete">X</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  };
+}
+
+const stateToProps = (state: StoreState): PropsState => {
+  return { todos: state.todos };
 };
+
+const actions: PropsActions = { fetchTodos };
+
+export const App = connect(stateToProps, actions)(AppComponent);
