7e50c16c4d8973ede7f5dea45ddefaf47c00651a
John <john.19col@gmail.com>
2019-12-01 23:49:12 -0500

    Add todos reducer to redux store reducers

--- src/state/reducers/todos.ts
+++ src/state/reducers/todos.ts
@@ -2,7 +2,7 @@ import { Todo } from './../../models/todo';
 import { FetchTodosAction } from './../actions/index';
 import { ActionType } from './../actions/types';
 
-export const todosReducer = (state: Todo[], action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
   switch (action.type) {
     case ActionType.FETCH_TODOS:
       return action.payload;
